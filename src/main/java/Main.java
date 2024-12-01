6534f62b6af8af3fd06ee7fdb4c6b5395a8c1639
Ahmed Al-Hasanat <ahmed.alhasanat@gmail.com>
2024-12-01 20:58:06 +0300

    Respond with 200

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,45 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
+        // Uncomment this block to pass the first stage
 
-     try {
-       ServerSocket serverSocket = new ServerSocket(4221);
+        try {
+            ServerSocket serverSocket = new ServerSocket(4221);
 
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
 
-       serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
-  }
+            Socket clientSocket = serverSocket.accept();// Wait for connection from client.
+            System.out.println("accepted new connection");
+
+            // Create input and output streams
+            InputStream input = clientSocket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+            OutputStream output = clientSocket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+
+            String request;
+            String response = "HTTP/1.1 200 OK\r\n\r\n";
+            while ((request = reader.readLine()) != null) {
+                System.out.println("Received: " + request);
+                writer.print(response);
+            }
+
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
 }
